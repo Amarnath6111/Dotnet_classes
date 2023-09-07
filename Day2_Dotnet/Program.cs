ac3db48b2c4a6a0a50ee3636b09721dcc921af30
Amarnath Mammidipally <amarnathma@infinite.com>
2023-09-08 00:54:05 +0530

    day3

---
+++ Day2_Dotnet/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace DotnetCoding1
+{
+    class Myclass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("small program");
+            int a = Convert.ToInt32(Console.ReadLine());
+            bool temp;
+            if (a == 10)
+            {
+                 temp = true;
+            }
+            else
+            {
+                temp = false;
+                    }
+            Console.WriteLine($" a = 10 is {temp}");
+            Myfun.Details();
+            Console.ReadKey();
+
+        }
+    }
+    class Myfun
+    {
+        public static void Details()
+        {
+            Console.WriteLine("enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("enter your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{name} is {age} Years old");
+            Console.WriteLine("My name is  {0} and I am {1} years old",name,age);
+        }
+
+    }
+}
---
+++ Day2_Dotnet/TypeConversion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetCoding1
+{
+    class TypeConversion
+    {
+        static void Main()
+        {
+            Console.WriteLine("enter employee name");
+            string name= Console.ReadLine();
+            Console.WriteLine("Enter employee ID");
+            int id = Convert.ToInt32(Console.ReadLine());
+            string a= "123";
+            int b;
+            b = int.Parse(a);
+            Console.WriteLine(b);
+            TypeConversion.Example();
+            Console.ReadLine();
+            
+        }
+        public static void Example()
+        {
+            double cgp;
+            Console.WriteLine("enter your btech cgp");
+            string i = Console.ReadLine();
+            bool val = double.TryParse(i, out cgp );
+            if (val) 
+            {
+                Console.WriteLine(cgp);
+            }
+            else
+            {
+                Console.WriteLine("error");
+
+            }
+
+
+        }
+    }
+}
