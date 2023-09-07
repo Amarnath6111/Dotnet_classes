f2abd9e850b1833774db007a45681c1d5fed800d
Amarnath Mammidipally <amarnathma@infinite.com>
2023-09-07 15:51:04 +0530

    day4_dotnetAss

---
+++ Day4_Dotnet/Day4_Dotnet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4_Dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter no of rows and columns");
+            int r = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int[,] arr1 = new int [r,c];
+            for (int i = 0; i < arr1.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr1.GetLength(1); j++)
+                {
+                    arr1[i,j]= Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    Console.Write(arr1[i,j]);
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
