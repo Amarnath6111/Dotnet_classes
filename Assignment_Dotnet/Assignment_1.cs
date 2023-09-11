851771ba88b4a77dbadbee5c18dbba37e486b322
Amarnath Mammidipally <amarnathma@infinite.com>
2023-09-11 12:02:57 +0530

    assignment

---
+++ Assignment_Dotnet/Assignment_1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.Equal();
+            Program.PosOrNeg();
+            Program.Operations();
+            Console.ReadLine();
+
+        }
+        static void Equal()
+        {
+            Console.WriteLine("enter two numbers");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            if (x == y)
+            {
+                Console.WriteLine($"{x} is equal to {y}");
+            }
+            else
+            {
+                Console.WriteLine($"{x} is not equal to {y}");
+            }
+
+        }
+        static void PosOrNeg()
+        {
+            Console.WriteLine($"enter a number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a>=0)
+            {
+                Console.WriteLine($"{a} is Positive");
+
+            }
+            else
+            {
+                Console.WriteLine($"{a} is Negative");
+            }
+        }
+        static void Operations()
+        {
+            Console.WriteLine("enter two numbers");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int sum = x + y;
+            Console.WriteLine($"{x} + {y}= {sum}");
+            int diff = x - y;
+            Console.WriteLine($"{x} - {y}= {diff}");
+            int mul = x * y;
+            Console.WriteLine($"{x} * {y}= {mul}");
+            int div = x / y;
+            Console.WriteLine($"{x} / {y}= {div}");
+
+
+
+        }
+    }
+}
