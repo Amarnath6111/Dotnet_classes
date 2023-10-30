67a7e8abe800921680a464a0a61948a55d1257cf
Amarnath6111 <144057606+Amarnath6111@users.noreply.github.com>
2023-10-30 12:22:42 +0530

    CodeBaseTest_6(procedure)

--- Sqltraining/Codebasetest_6/CodeBaseTestSql_6.sql
+++ Sqltraining/Codebasetest_6/CodeBaseTestSql_6.sql
@@ -6,4 +6,11 @@ empname varchar(35) unique,
 empsal numeric(10,2) check (empsal>=25000),
 emptype char(1))
 ----
-create or alter procedure AddRecord (@empname varchar(35),@empsal numeric(10,2),@emptype char(1))
+create or alter procedure AddRecord (@empname varchar(35),@empsal int,@emptype char(1))
+as
+begin
+insert into CodeEmployee values(@empname,@empsal,@emptype)
+select * from CodeEmployee
+end
+
+exec AddRecord 'Amarnath',27000,'p'
